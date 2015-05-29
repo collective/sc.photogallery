98327010fd248b8c2f20f40335414c822c22bf70
hvelarde <hector.velarde@gmail.com>
2015-05-29 19:09:43 -0300

    Fix tile installation/removal
    - Fix installation when plone.app.tiles was not previously installed
    - Fix removal

--- src/sc/photogallery/Extensions/Install.py
+++ src/sc/photogallery/Extensions/Install.py
@@ -3,8 +3,15 @@ from sc.photogallery.config import PROJECTNAME
 from plone import api
 
 
+def remove_tile(portal):
+    tiles = api.portal.get_registry_record('plone.app.tiles')
+    if u'sc.photogallery' in tiles:
+        tiles.remove(u'sc.photogallery')
+
+
 def uninstall(portal, reinstall=False):
     if not reinstall:
+        remove_tile(portal)
         profile = 'profile-{0}:uninstall'.format(PROJECTNAME)
         setup_tool = api.portal.get_tool('portal_setup')
         setup_tool.runAllImportStepsFromProfile(profile)
