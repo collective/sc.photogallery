9c82a4de1535c6f5fbf002f34ecc57d66e4ddca0
hvelarde <hector.velarde@gmail.com>
2015-06-01 08:54:46 -0300

    Fix upgrade step

--- src/sc/photogallery/upgrades/v1001/__init__.py
+++ src/sc/photogallery/upgrades/v1001/__init__.py
@@ -6,8 +6,8 @@ import logging
 
 
 def apply_profile(context):
-    """Updates package to profile version 1."""
+    """Updates package to profile version 1001."""
     logger = logging.getLogger(PROJECTNAME)
-    profile = 'profile-sc.photogallery.upgrades.v1:default'
+    profile = 'profile-sc.photogallery.upgrades.v1001:default'
     loadMigrationProfile(context, profile)
-    logger.info('Profile updated to version 1')
+    logger.info('Profile updated to version 1001')
